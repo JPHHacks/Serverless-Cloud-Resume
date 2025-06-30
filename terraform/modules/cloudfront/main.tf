78f664abf5248ffc0dd236a8fc24c2e1db1e7f73
JPHHacks <jpheymann@protonmail.com>
2025-06-30 00:33:18 -0400

    test

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -93,6 +93,22 @@ resource "aws_s3_bucket_policy" "access_logs" {
             "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/*"
           }
         }
+      },
+      {
+        Sid    = "CloudFrontLogDeliveryAcl"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = [
+          "s3:GetBucketAcl"
+        ]
+        Resource = aws_s3_bucket.access_logs.arn
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/*"
+          }
+        }
       }
     ]
   })
